9fe827a81d29a4ea208735a3c9674de5c1f0886e
Tom Brennan <tommybrennan@gmail.com>
2014-05-22 14:00:02 -0400

    Creating tickets I still have two test failures that I am working through

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
@@ -14,7 +16,7 @@ class ProjectsController < ApplicationController
       flash[:notice] = "Project has been created."
       redirect_to @project
     else
-      flash[:alert] = "Project has not been created"
+      flash[:alert] = "Project has not been created."
       render "new"
     end
   end
@@ -51,6 +53,14 @@ class ProjectsController < ApplicationController
 
     def project_params
       params.require(:project).permit(:name, :description)
+    end
+
+    def set_project
+      @project = Project.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The project you were looking" +
+                      " for could not be found."
+      redirect_to projects_path
     end
 
 end
